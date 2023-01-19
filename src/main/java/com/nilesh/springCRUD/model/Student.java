1ce62504b6132b0d827d70d950969d7383d48428
Sahilrajkaraval <sahilkaraval@gmail.com>
2023-01-19 11:51:45 +0530

    Initial commit

---
+++ src/main/java/com/nilesh/springCRUD/model/Student.java
@@ -0,0 +1,51 @@
+package com.nilesh.springCRUD.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name ="student")
+public class Student {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long studentId;
+	private String studentName;
+	private String schoolName;
+	private String className;
+
+
+	public Long getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(Long studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public String getSchoolName() {
+		return schoolName;
+	}
+
+	public void setSchoolName(String schoolName) {
+		this.schoolName = schoolName;
+	}
+
+	public String getClassName() {
+		return className;
+	}
+
+	public void setClassName(String className) {
+		this.className = className;
+	}
+}
